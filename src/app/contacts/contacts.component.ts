6ae02833d855ac501d88a7b6fba67fa83ee11ceb
racko92 <vladimir.rackovic.92@gmail.com>
2017-08-21 21:14:15 +0200

    Dodat objekat u .ts, prikazan u .html-u

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-contacts',
-    template: '<h1>Contacts</h1>'
+    templateUrl: './contacts.component.html'
 })
 export class ContactsComponent {
 
     constructor() { }
+
+    private contact = {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'john@doe.com'
+    }
 }
