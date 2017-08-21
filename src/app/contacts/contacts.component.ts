0900b30f6789c60764f969867a407cd8b482b516
racko92 <vladimir.rackovic.92@gmail.com>
2017-08-21 20:54:26 +0200

    Making my first component

---
+++ src/app/contacts/contacts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-contacts',
+    template: '<h1>Contacts</h1>'
+})
+export class ContactsComponent {
+
+    constructor() { }
+}
